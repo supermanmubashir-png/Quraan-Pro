4927688189c13736dc9502cdd25d6121c03b3dbc
supermanmubashir-png <superman.mubashir@gmail.com>
2026-04-02 17:35:31 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE="quran-pro-v2";
+
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll([
+   "./",
+   "./index.html",
+   "./manifest.json"
+  ]))
+ );
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
+});
