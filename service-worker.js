5a1a6de2bf5255d412b15fdcefcf7d68520912b8
supermanmubashir-png <superman.mubashir@gmail.com>
2026-05-05 20:51:13 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,3 @@
-const CACHE="quran-pro-v5";
-
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
-  "./","./index.html","./icon.png"
- ])));
-});
-
-self.addEventListener("activate",e=>{
- e.waitUntil(caches.keys().then(keys=>{
-  return Promise.all(keys.map(k=>{
-   if(k!==CACHE) return caches.delete(k);
-  }));
- }));
-});
-
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener("fetch", e=>{
+  return fetch(e.request);
 });
