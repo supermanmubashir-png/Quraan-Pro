de64e3f3c34003bf100b243282abf628d1aa40d6
supermanmubashir-png <superman.mubashir@gmail.com>
2026-05-10 18:43:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -81,3 +81,10 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request, { cache: "no-store" })
+      .catch(() => caches.match(event.request))
+  );
+});
