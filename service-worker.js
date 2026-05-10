4845b49cc0bc98ccfb92c4caabd09b00dddb2690
supermanmubashir-png <superman.mubashir@gmail.com>
2026-05-10 18:22:20 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -59,3 +59,25 @@ self.addEventListener("fetch", event => {
       })
   );
 });
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
